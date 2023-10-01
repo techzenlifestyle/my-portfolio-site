29b5856dccb67be23b90bb5064a7d88ab6953a3a
TechZenCreator <kevin@hiddengeniusproject.org>
2023-10-01 06:55:52 -0700

    testing contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
-function onSubmit(token) {
-    document.getElementById("contact-form").submit();
-}
+grecaptcha.ready(function() {
+    grecaptcha.execute('6LfAr2koAAAAAJtczSiaJ8X2l_I_DiCl6wTRSCMR', {action: 'homepage'})
+    .then(function(token) {
+      document.getElementById('captchaResponse').value = token;
+    });
+  });
